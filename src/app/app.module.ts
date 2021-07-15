e8de59e0d926d54c87a3edb6041cc7bd718766f9
Hema35 <cheershema@gmail.com>
2021-07-15 17:56:25 -0400

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NameEditorComponent } from './name-editor/name-editor.component';
+import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NameEditorComponent,
+    ProfileEditorComponent,
 
   ],
   imports: [
