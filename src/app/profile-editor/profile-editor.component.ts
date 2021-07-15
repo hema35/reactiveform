0d52a8e0946fe32108f265ab3b38ab672b0371de
Hema35 <cheershema@gmail.com>
2021-07-15 18:27:17 -0400

    nested forms

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -10,11 +10,26 @@ export class ProfileEditorComponent implements OnInit {
 profileForm = new FormGroup({
   firstName: new FormControl(''),
   lastName: new FormControl(''),
+  address: new FormGroup({
+    street: new FormControl(''),
+    city: new FormControl(''),
+    state: new FormControl(''),
+    zip: new FormControl('')
+  })
 })
 onSubmit()
 {
   console.warn(this.profileForm.value);
 }
+updateProfile(){
+  this.profileForm.patchValue({
+    firstName: 'Nancy',
+    address: {
+      street: '123 Drew street',
+    }
+  });
+}
+
   constructor() { }
 
   ngOnInit(): void {
